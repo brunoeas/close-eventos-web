d0b2e414d530192e1ee7c8bd7ccbbb713bf95068
Bruno Eduardo <bruno.soares@kepha.com.br>
2020-07-20 17:59:49 -0300

    Foto do usuário e alguns ajustes e correções

--- close-eventos/src/views/usuario/usuario.styles.ts
+++ close-eventos/src/views/usuario/usuario.styles.ts
@@ -19,6 +19,19 @@ const useStyles = makeStyles((theme) => ({
     },
     marginRight: 15,
   },
+  inputFile: {
+    display: 'none',
+    width: 0,
+    height: 0,
+  },
+  buttonRemoveImg: {
+    color: 'white',
+    backgroundColor: theme.palette.info.main,
+    '&:hover': {
+      backgroundColor: theme.palette.info.dark,
+    },
+    marginRight: 15,
+  },
 }));
 
 export { useStyles };
